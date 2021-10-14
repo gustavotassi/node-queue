3e342f62cc905eef71b4ee82fac11e4e46c306eb
gustavotassi <souzagustavotassi@gmail.com>
2021-10-13 23:03:42 -0300

    feat(server): add bull board to view queues better

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,16 @@
+import BullBoard from 'bull-board';
 import express, { json } from 'express';
 import * as UserController from './app/controllers/UserController';
+import { queues } from './app/lib/Queue';
 
 const app = express();
+BullBoard.setQueues(queues.map((queue) => queue.bull));
 
 app.use(json());
 
 app.post('/users', UserController.store);
+
+app.use('/admin/queues', BullBoard.UI);
 
 app.listen(3000, () => {
     console.log(`Server is running on port 3000!`);
